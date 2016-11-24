4ad64a1c119bda256e36d2863ae8e03c09074979
Joabe Mendes <joab.mendes.r2d2@gmail.com>
2016-11-24 01:11:32 -0300

    Fixing style wip #4

--- tests/test.py
+++ tests/test.py
@@ -10,13 +10,12 @@ class FbbotwTest(unittest.TestCase):
         self.fbid = os.getenv('FBID', '')
 
     def test_typing(self):
-        response = fbbotw.typing(fbid=self.fbid, sender_action="typing_on").json()
-        self.assertTrue(response['recipient_id'] == self.fbid)
-        response = fbbotw.typing(fbid=self.fbid, sender_action="typing_off").json()
-        self.assertTrue(response['recipient_id'] == self.fbid)
-        response = fbbotw.typing(fbid=self.fbid, sender_action="mark_seen").json()
-        self.assertTrue(response['recipient_id'] == self.fbid)
-
+        response = fbbotw.typing(fbid=self.fbid, sender_action="typing_on")
+        self.assertTrue(response.json()['recipient_id'] == self.fbid)
+        response = fbbotw.typing(fbid=self.fbid, sender_action="typing_off")
+        self.assertTrue(response.json()['recipient_id'] == self.fbid)
+        response = fbbotw.typing(fbid=self.fbid, sender_action="mark_seen")
+        self.assertTrue(response.json()['recipient_id'] == self.fbid)
 
 
 if __name__ == '__main__':
