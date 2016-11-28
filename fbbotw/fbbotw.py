0732918c016004bd254b211506c6ebe8ea8411f1
Joabe Mendes <joab.mendes.r2d2@gmail.com>
2016-11-28 17:45:21 -0300

    Covered send api for simple templates. Closes #5

--- fbbotw/fbbotw.py
+++ fbbotw/fbbotw.py
@@ -31,8 +31,8 @@ MSG_URL = 'https://graph.facebook.com/v2.6/me/messages?access_token='
 
 def get_user_information(fbid):
     """ Gets user information: first_name, last_name, gender, profile_pic,
-    locale, timezone, is_payment_enabled.
-    /docs/messenger-platform/user-profile
+    locale, timezone, is_payment_enabled
+    (/docs/messenger-platform/user-profile).
 
     :param str fbid: User id to get the information
     :return: dict with keys
@@ -77,7 +77,7 @@ def post_settings(greeting_text):
 
 def post_greeting_text(greeting_text):
     """ Sets the Thread Settings Greeting Text
-    /docs/messenger-platform/thread-settings/greeting-text
+    (/docs/messenger-platform/thread-settings/greeting-text).
 
     :param str greeting_text: Desired Greeting Text (160 chars)
     :return: Response object
@@ -93,7 +93,7 @@ def post_greeting_text(greeting_text):
 
 def post_start_button(payload='START'):
     """ Sets the Thread Settings Greeting Text
-    /docs/messenger-platform/thread-settings/get-started-button
+    (/docs/messenger-platform/thread-settings/get-started-button).
 
     :param str payload: Desired postback payload (Default START)
     :return: Response object
@@ -110,23 +110,24 @@ def post_start_button(payload='START'):
 
 def post_persistent_menu(call_to_actions):
     """ Sets a persistent menu on the chat
-    /docs/messenger-platform/thread-settings/persistent-menu
+    (/docs/messenger-platform/thread-settings/persistent-menu)
 
-    :param list call_to_actions: example
-    call_to_actions =  [
-        {
-            'type': 'postback',
-            'title': 'About',
-            'payload': 'ABOUT'
-        },
-        {
-            'type': 'web_url',
-            'title': 'Google it',
-            'url': 'https://www.google.com',
-            'webview_height_ratio': 'full',
-            'messenger_extensions': True
-        },
-    ]
+    :param list call_to_actions: format :
+
+        >>> call_to_actions =  [
+            {
+                'type': 'postback',
+                'title': 'About',
+                'payload': 'ABOUT'
+            },
+            {
+                'type': 'web_url',
+                'title': 'Google it',
+                'url': 'https://www.google.com',
+                'webview_height_ratio': 'full',
+                'messenger_extensions': True
+            },
+        ]
     :return: Response object
     """
     url = TD_STS_URL + PAGE_ACCESS_TOKEN
@@ -134,6 +135,40 @@ def post_persistent_menu(call_to_actions):
     payload["setting_type"] = "call_to_actions"
     payload["thread_state"] = "existing_thread"
     payload["call_to_actions"] = call_to_actions
+    data = json.dumps(payload)
+    status = requests.post(url, headers=HEADER, data=data)
+    return status
+
+
+def post_domain_whitelisting(whitelisted_domains, domain_action_type='add'):
+    """ Sets the whistelisted domains for the Messenger Extension
+    (/docs/messenger-platform/thread-settings/domain-whitelisting).
+
+    :param list whistelisted_domains: Domains to be whistelisted
+    :param str domain_action_type: Action to run `add/remove` (Defaut add)
+    :return: Response object
+    """
+    url = TD_STS_URL + PAGE_ACCESS_TOKEN
+    payload = {}
+    payload['setting_type'] = 'domain_whitelisting'
+    payload['whitelisted_domains'] = whitelisted_domains
+    payload['domain_action_type'] = domain_action_type
+    data = json.dumps(payload)
+    status = requests.post(url, headers=HEADER, data=data)
+    return status
+
+
+def post_account_linking_url(account_linking_url):
+    """ Sets the liking url to link the user with your business login
+    (/docs/messenger-platform/thread-settings/account-linking).
+
+    :param str account_linking_url: URL to the account linking OAuth flow
+    :return: Response object
+    """
+    url = TD_STS_URL + PAGE_ACCESS_TOKEN
+    payload = {}
+    payload['setting_type'] = 'account_linking'
+    payload['account_linking_url'] = account_linking_url
     data = json.dumps(payload)
     status = requests.post(url, headers=HEADER, data=data)
     return status
@@ -147,10 +182,10 @@ def post_persistent_menu(call_to_actions):
 
 def typing(fbid, sender_action):
     """ Displays/Hides the typing gif/mark seen on facebook chat
-    /docs/messenger-platform/send-api-reference/sender-actions
+    (/docs/messenger-platform/send-api-reference/sender-actions)
 
-    :param str fbid: User id to display action
-    :param str sender_action: typing_off/typing_on/mark_seen
+    :param str fbid: User id to display action.
+    :param str sender_action: `typing_off/typing_on/mark_seen`.
     :return: Response object
     """
     url = MSG_URL + PAGE_ACCESS_TOKEN
@@ -161,15 +196,16 @@ def typing(fbid, sender_action):
     status = requests.post(url, headers=HEADER, data=data)
     return status
 
+
 # Send API Content Type
 
 
 def post_text_message(fbid, message):
     """ Sends a common text message
-    /docs/messenger-platform/send-api-reference/text-message
+    (/docs/messenger-platform/send-api-reference/text-message)
 
     :param str fbid: User id to get the text.
-    :param str message: Text to be displayed for the user (230 chars)
+    :param str message: Text to be displayed for the user (230 chars).
     :return: Response object
     """
     url = MSG_URL + PAGE_ACCESS_TOKEN
@@ -183,7 +219,7 @@ def post_text_message(fbid, message):
 
 def post_attachment(fbid, media_url, file_type):
     """ Sends a media attachment
-    /docs/messenger-platform/send-api-reference/contenttypes
+    (/docs/messenger-platform/send-api-reference/contenttypes)
 
     :param str fbid: User id to send the audio.
     :param str url: Url of a hosted media.
@@ -202,7 +238,7 @@ def post_attachment(fbid, media_url, file_type):
 
 def post_audio_attachment(fbid, audio_url):
     """ Sends an audio attachment
-    /docs/messenger-platform/send-api-reference/audio-attachment
+    (/docs/messenger-platform/send-api-reference/audio-attachment)
 
     :param str fbid: User id to send the audio.
     :para str audio_url: Url of a hosted audio (10 Mb).
@@ -213,7 +249,7 @@ def post_audio_attachment(fbid, audio_url):
 
 def post_file_attachment(fbid, file_url):
     """ Sends a file attachment
-    /docs/messenger-platform/send-api-reference/file-attachment
+    (/docs/messenger-platform/send-api-reference/file-attachment)
 
     :param str fbid: User id to send the file.
     :para str file_url: Url of a hosted file (10 Mb).
@@ -224,7 +260,7 @@ def post_file_attachment(fbid, file_url):
 
 def post_image_attachment(fbid, img_url):
     """ Sends an image attachment
-    /docs/messenger-platform/send-api-reference/image-attachment
+    (/docs/messenger-platform/send-api-reference/image-attachment)
 
     :param str fbid: User id to send the image.
     :para str img_url: Url of a hosted image (jpg, png, gif).
@@ -235,7 +271,7 @@ def post_image_attachment(fbid, img_url):
 
 def post_video_attachment(fbid, video_url):
     """ Sends a video attachment
-    /docs/messenger-platform/send-api-reference/video-attachment
+    (/docs/messenger-platform/send-api-reference/video-attachment)
 
     :param str fbid: User id to send the video.
     :para str video_url: Url of a hosted video.
@@ -248,12 +284,13 @@ def post_video_attachment(fbid, video_url):
 
 def post_text_w_quickreplies(fbid, message, quick_replies):
     """ Send text with quick replies buttons
-    /docs/messenger-platform/send-api-reference/quick-replies
+    (/docs/messenger-platform/send-api-reference/quick-replies).
 
     :param str fbid: User id to send the quick replies menu.
     :param str message: message to be displayed with the menu.
-    :param list quick_replies: example
-        quick_replies = [
+    :param list quick_replies: (Max 10) format :
+
+        >>> quick_replies = [
             {
                 "content_type": "text",
                 "title": "Yes!",
@@ -277,3 +314,234 @@ def post_text_w_quickreplies(fbid, message, quick_replies):
 
 
 # Send API Templates
+
+def post_button_template(fbid, text, buttons):
+    """ Sends a button template with the specified text and buttons
+    (/docs/messenger-platform/send-api-reference/button-template).
+
+    :param str fbid: User id to send the buttons.
+    :param str text: Message to be displayed with the buttons (320 Chars).
+    :param list buttons: Dict of buttons that appear as call-to-actions -
+    format :
+
+        >>> buttons = [
+            {
+                'type': 'web_url',
+                'url': 'https://petersapparel.parseapp.com',
+                'title': 'Show Website'
+            },
+            {
+                'type': 'postback',
+                'title': 'Start Chatting',
+                'payload': 'USER_DEFINED_PAYLOAD'
+            }
+        ]
+    :return: Response object.
+    """
+    url = MSG_URL + PAGE_ACCESS_TOKEN
+    data = {}
+    data['recipient'] = {'id': fbid}
+    payload = {}
+    payload['template_type'] = 'button'
+    payload['text'] = text
+    payload['buttons'] = buttons
+    attachment = {"type": 'template', "payload": payload}
+    data['message'] = {"attachment": attachment}
+    data = json.dumps(data)
+    status = requests.post(url, headers=HEADER, data=data)
+    return status
+
+
+def post_generic_template(fbid, title, item_url='', image_url='', subtitle='',
+                          buttons=[]):
+    """ Sends a generic template for the specified User
+    (/docs/messenger-platform/send-api-reference/generic-template).
+
+    :param str fbid: User id to send the generic template.
+    :param str title: Bubble title (80 Chars).
+    :param str item_url: URL that is opened when bubble is tapped.
+    :param str image_url: Bubble image (Ratio 1.91:1).
+    :param str subtitle: Bubble subtitle (80 Chars).
+    :param list buttons: (Max 3) format :
+
+        >>> buttons = [
+            {
+                'type': 'web_url',
+                'url': 'https://petersfancybrownhats.com',
+                'title': 'View Website'
+            },
+            {
+                'type': 'postback',
+                'title': 'Start Chatting',
+                'payload': 'DEVELOPER_DEFINED_PAYLOAD'
+            }
+        ]
+    :return: Response object
+    """
+    url = MSG_URL + PAGE_ACCESS_TOKEN
+    data = {}
+    data['recipient'] = {'id': fbid}
+    payload = {}
+    payload['template_type'] = 'generic'
+    payload['elements'] = []
+    element = {}
+    element['title'] = title
+    element['item_url'] = item_url
+    element['image_url'] = image_url
+    element['subtitle'] = subtitle
+    element['buttons'] = buttons
+    payload['elements'].append(element)
+    attachment = {"type": 'template', "payload": payload}
+    data['message'] = {"attachment": attachment}
+    data = json.dumps(data)
+    status = requests.post(url, headers=HEADER, data=data)
+    return status
+
+
+def post_list_template(fbid, elements, buttons=[], top_element_style='large'):
+    """ Sends a list template for the specified User
+    (/docs/messenger-platform/send-api-reference/list-template)
+
+    :param str fbid: User id to send the list template.
+    :param str top_element_style: large/compact (Default Large).
+    :param list elements: List view elements (Max 4/Min 2 elements)
+    format :
+
+        >>> elements = [{
+            'title': 'Classic Gray T-Shirt',
+            'image_url': 'https://app.ngrok.io/img/gray-t-shirt.png',
+            'subtitle': '100% Cotton, 200% Comfortable',
+            'default_action': {
+                'type': 'web_url',
+                'url': 'https://app.ngrok.io/view?item=103',
+                'messenger_extensions': true,
+                'webview_height_ratio': 'tall',
+                'fallback_url': 'https://peterssendreceiveapp.ngrok.io/'
+            },
+            'buttons': [
+                {
+                    'title': 'Buy',
+                    'type': 'web_url',
+                    'url': 'https://app.ngrok.io/shop?item=103',
+                    'messenger_extensions': true,
+                    'webview_height_ratio': 'tall',
+                    'fallback_url': 'https://app.ngrok.io/'
+                }
+            ]
+        }]
+
+    :param list buttons: List of buttons associated on the list
+    template message (Max 1). fomart :
+
+        >>> buttons = [
+            {
+                'title': 'View More',
+                'type': 'postback',
+                'payload': 'payload'
+            }
+        ]
+    :return: Response object
+    """
+    url = MSG_URL + PAGE_ACCESS_TOKEN
+    data = {}
+    data['recipient'] = {'id': fbid}
+    payload = {}
+    payload['template_type'] = 'list'
+    payload['top_element_style'] = top_element_style
+    payload['buttons'] = buttons
+    payload['elements'] = elements
+    attachment = {"type": 'template', "payload": payload}
+    data['message'] = {"attachment": attachment}
+    data = json.dumps(data)
+    status = requests.post(url, headers=HEADER, data=data)
+    return status
+
+
+def post_receipt_template(fbid, recipient_name, order_number, currency,
+                          payment_method, summary, merchant_name='',
+                          timestamp='', order_url='', elements=[],
+                          address={}, adjustments=[]):
+    """ Sends a receipt template for the specified user
+    /docs/messenger-platform/send-api-reference/receipt-template
+
+    :param str fbid: User id to send the receipt template.
+    :param str recipient_name: Recipient's name.
+    :param str order_number: Unique order number.
+    :param str currency: Currency for order.
+    :param str payment_method: Payment method details. ex - "Visa 1234".
+    :param dict summary: Payment summary, format :
+
+        >>> summary = {
+          "subtotal": 75.00,
+          "shipping_cost": 4.95,
+          "total_tax": 6.19,
+          "total_cost": 56.14
+        }
+    :param str timestamp: Timestamp of the order, in seconds.
+    :param str order_url: URL of order.
+    :param list elements: Items in order (Max 100), format :
+
+        >>> elements = [
+          {
+            "title": "Classic White T-Shirt",
+            "subtitle": "100% Soft and Luxurious Cotton",
+            "quantity": 2,
+            "price": 50,
+            "currency": "USD",
+            "image_url": "http://i.imgur.com/GHC4ZHl.jpg"
+          },
+          {
+            "title": "Classic Gray T-Shirt",
+            "subtitle": "100% Soft and Luxurious Cotton",
+            "quantity": 1,
+            "price": 25,
+            "currency": "USD",
+            "image_url": "http://i.imgur.com/GHC4ZHl.jpg"
+          }
+        ],
+    :param dict address: Shipping address (If order has shipping),
+    format :
+
+        >>> address = {
+          "street_1": "1 Hacker Way",
+          "street_2": "",
+          "city": "Menlo Park",
+          "postal_code": "94025",
+          "state": "CA",
+          "country": "US"
+        }
+    :param list adjustments: Payment adjustments, format :
+
+        >>> adjustments = [
+          {
+            "name":"New Customer Discount",
+            "amount":20
+          },
+          {
+            "name":"$10 Off Coupon",
+            "amount":10
+          }
+        ]
+    :return: Response object
+    """
+    url = MSG_URL + PAGE_ACCESS_TOKEN
+    data = {}
+    data['recipient'] = {'id': fbid}
+    payload = {}
+    payload['template_type'] = 'receipt'
+    payload['recipient_name'] = recipient_name
+    payload['order_number'] = order_number
+    payload['currency'] = currency
+    payload['payment_method'] = payment_method
+    payload['summary'] = summary
+    payload['order_url'] = order_url
+    payload['timestamp'] = timestamp
+    payload['elements'] = elements
+    payload['address'] = address
+    payload['adjustments'] = adjustments
+    payload['merchant_name'] = merchant_name
+    attachment = {"type": "template", "payload": payload}
+    data['message'] = {"attachment": attachment}
+    data = json.dumps(data)
+    status = requests.post(url, headers=HEADER, data=data)
+    return status
--- tests/test.py
+++ tests/test.py
@@ -8,6 +8,28 @@ class FbbotwTest(unittest.TestCase):
 
     def setUp(self):
         self.fbid = os.getenv('FBID', '')
+        self.postback_buttons = [
+            {
+                'type': 'postback',
+                'title': 'Yes!',
+                'payload': 'USER_SAY_YES'
+            },
+            {
+                'type': 'postback',
+                'title': 'Nope',
+                'payload': 'USER_SAY_NOT'
+            }
+        ]
+
+    #############################################
+    #           Graph API Functions             #
+    #############################################
+
+    def test_get_user_info(self):
+        response = fbbotw.get_user_information(fbid=self.fbid)
+        self.assertTrue(response['first_name'] == 'Rto')
+        self.assertTrue(response['last_name'] == 'Dto')
+        self.assertTrue(response['gender'] == 'male')
 
     #############################################
     #        Tread Settings Functions           #
@@ -26,24 +48,24 @@ class FbbotwTest(unittest.TestCase):
         self.assertTrue(response.status_code == 200)
 
     def test_post_persistent_menu(self):
-        call_to_actions = [
-            {
-                'type': 'postback',
-                'title': 'About',
-                'payload': 'ABOUT'
-            },
-            {
-                'type': 'postback',
-                'title': 'Help',
-                'payload': 'HELP'
-            },
-        ]
-        response = fbbotw.post_persistent_menu(call_to_actions)
+        response = fbbotw.post_persistent_menu(self.postback_buttons)
         self.assertTrue(response.status_code == 200)
+
+    def test_post_domain_whitelisting(self):
+        domain = ['https://breco.herokuapp.com']
+        response = fbbotw.post_domain_whitelisting(whitelisted_domains=domain)
+        self.assertTrue(response.status_code == 200)
+
+    # def test_post_account_linking_url(self):
+    #    url = ''
+    #    response = fbbotw.post_account_linking_url(account_linking_url=url)
+    #    self.assertTrue(response.status_code == 200)
 
     #############################################
     #        Tread Settings Functions           #
     #############################################
+
+    # Send API Sender Actions
 
     def test_typing(self):
         # Test typing_on option
@@ -59,11 +81,7 @@ class FbbotwTest(unittest.TestCase):
         self.assertTrue(response.json()['recipient_id'] == self.fbid)
         self.assertTrue(response.status_code == 200)
 
-    def test_get_user_info(self):
-        response = fbbotw.get_user_information(fbid=self.fbid)
-        self.assertTrue(response['first_name'] == 'Rto')
-        self.assertTrue(response['last_name'] == 'Dto')
-        self.assertTrue(response['gender'] == 'male')
+    # Send API Content Type
 
     def test_post_text_message(self):
         response = fbbotw.post_text_message(fbid=self.fbid, message="Hello")
@@ -94,24 +112,134 @@ class FbbotwTest(unittest.TestCase):
         self.assertTrue(response.status_code == 200)
         self.assertTrue(response.json()['recipient_id'] == self.fbid)
 
+    # Send API Quick Replies
+
     def test_post_text_w_quickreplies(self):
         quick_replies = [
             {
-                "content_type": "text",
-                "title": "Yes!",
-                "payload": "USER_SAY_YES"
+                'content_type': 'text',
+                'title': 'Yes!',
+                'payload': 'USER_SAY_YES'
             },
             {
-                "content_type": "text",
-                "title": "Nope",
-                "payload": "USER_SAY_NOT"
+                'content_type': 'text',
+                'title': 'Nope',
+                'payload': 'USER_SAY_NOT'
             }
         ]
         response = fbbotw.post_text_w_quickreplies(fbid=self.fbid,
-                                                   message="Test",
+                                                   message='Test?',
                                                    quick_replies=quick_replies)
         self.assertTrue(response.status_code == 200)
         self.assertTrue(response.json()['recipient_id'] == self.fbid)
+
+    def test_post_button_template(self):
+        response = fbbotw.post_button_template(fbid=self.fbid, text="Test?",
+                                               buttons=self.postback_buttons)
+        self.assertTrue(response.status_code == 200)
+        self.assertTrue(response.json()['recipient_id'] == self.fbid)
+
+    def test_post_generic_template(self):
+        title = 'This is a Generic Template'
+        item_url = 'http://breco.herokuapp.com/'
+        image_url = 'https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg'
+        subtitle = 'Generic Template Subtitle'
+        response = fbbotw.post_generic_template(fbid=self.fbid,
+                                                title=title,
+                                                item_url=item_url,
+                                                image_url=image_url,
+                                                subtitle=subtitle,
+                                                buttons=self.postback_buttons)
+        self.assertTrue(response.status_code == 200)
+        self.assertTrue(response.json()['recipient_id'] == self.fbid)
+
+    def test_post_list_template(self):
+        elements = []
+        el = {
+            'title': 'Classic White T-Shirt',
+            'image_url': 'http://i.imgur.com/GHC4ZHl.jpg',
+            'subtitle': 'Nice shirt',
+            'default_action': {
+                'type': 'web_url',
+                'url': 'https://breco.herokuapp.com/',
+                'messenger_extensions': True,
+                'webview_height_ratio': 'tall',
+                'fallback_url': 'https://breco.herokuapp.com/'
+            },
+            'buttons': [{
+                'title': 'Buy',
+                'type': 'web_url',
+                'url': 'https://breco.herokuapp.com/',
+                'messenger_extensions': True,
+                'webview_height_ratio': 'tall',
+                'fallback_url': 'https://breco.herokuapp.com/'
+            }]
+        }
+        elements.append(el)
+        el['title'] = "Anothe Shirt"
+        elements.append(el)
+        buttons = [
+            {
+                'title': 'View More',
+                'type': 'postback',
+                'payload': 'VIEW_MORE'
+            }
+        ]
+        response = fbbotw.post_list_template(fbid=self.fbid,
+                                             elements=elements,
+                                             buttons=buttons)
+        self.assertTrue(response.status_code == 200)
+
+    def test_post_receipt_template(self):
+        name = "Joab Mendes"
+        order_number = "97892"
+        currency = "USD"
+        payment_method = "VISA 9852"
+        summary = {
+            "subtotal": 40.00,
+            "shipping_cost": 4.95,
+            "total_tax": 6.19,
+            "total_cost": 61.11
+        }
+        timestamp = '1480316409'
+        order_url = 'https://breco.herokuapp.com'
+        elements = [
+          {
+            "title": "Classic White T-Shirt",
+            "subtitle": "100% Soft and Luxurious Cotton",
+            "quantity": 2,
+            "price": 50,
+            "currency": "USD",
+            "image_url": "http://i.imgur.com/GHC4ZHl.jpg"
+          }
+        ]
+        address = {
+          "street_1": "1 Hacker Way",
+          "street_2": "",
+          "city": "Menlo Park",
+          "postal_code": "94025",
+          "state": "CA",
+          "country": "US"
+        }
+        adjustments = [
+            {
+                "name": "$10 Off Coupon",
+                "amount": 10
+            }
+        ]
+
+        response = fbbotw.post_receipt_template(fbid=self.fbid,
+                                                recipient_name=name,
+                                                order_number=order_number,
+                                                currency=currency,
+                                                payment_method=payment_method,
+                                                summary=summary,
+                                                timestamp=timestamp,
+                                                order_url=order_url,
+                                                elements=elements,
+                                                address=address,
+                                                adjustments=adjustments)
+        self.assertTrue(response.status_code == 200)
 
 
 if __name__ == '__main__':
