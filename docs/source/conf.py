eb471c20a364c9339798e1d91cd0947f67ef99d9
JoabMendes <joab.mendes.r2d2@gmail.com>
2017-06-20 21:32:48 -0300

    Fixing docs error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,6 @@
 
 import sys
 import os
-import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath('../../fbbotw'))
 
@@ -127,7 +126,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -136,13 +135,7 @@ html_theme = 'sphinx_rtd_theme'
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-html_theme_options = {
-    'collapse_navigation': True,
-    'display_version': True,
-    'navigation_depth': 1,
-}
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
